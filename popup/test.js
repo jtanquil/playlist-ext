675a3f2da846eb6dd3ec3e4a25fc4a309031ee21
jtanquil <jose.tanquiilut@gmail.com>
2024-06-02 13:23:36 -0700

    added iframe

--- popup/test.js
+++ popup/test.js
@@ -1,15 +1,21 @@
 let test_playlist = [
-  "https://www.youtube.com/watch?v=G8voJ0xK5pc",
-  "https://www.youtube.com/watch?v=DmStNF35o1w",
-  "https://www.youtube.com/watch?v=ZCtss8CCdSI",
+  "https://www.youtube.com/embed/G8voJ0xK5pc",
+  "https://www.youtube.com/embed/DmStNF35o1w",
+  "https://www.youtube.com/embed/ZCtss8CCdSI",
 ];
 
 window.onload = () => {
+  const player = document.createElement("iframe");
+  player.src = "https://www.youtube.com/embed/ZCtss8CCdSI";
+
   const list = document.createElement("ul");
+  list.className += "playlist";
 
   test_playlist.forEach((item, index) => {
     const listItem = document.createElement("li");
     const playlistLink = document.createElement("a");
+
+    listItem.className += "playlist-item";
     
     playlistLink.href = item;
     playlistLink.appendChild(document.createTextNode(`${index}: ${item}`));
@@ -18,6 +24,26 @@ window.onload = () => {
     list.appendChild(listItem);
   });
 
+  document.body.appendChild(player);
   document.body.appendChild(list);
 };
 
+window.addEventListener("load", (event) => {
+  const playlist = document.querySelector(".playlist");
+
+  if (!!playlist.children && playlist.children.length > 0) {
+    const playlistItems = playlist.children;
+  
+    for (const playlistItem of playlistItems) {
+      const link = playlistItem.firstChild;
+
+      link.addEventListener("click", (event) => {
+        const player = document.querySelector("iframe");
+        player.src = playlistItem.firstChild.href;
+
+        // const button = document.querySelector("yt-large-play-button");
+        // button.click();
+      });
+    }
+  }
+});
